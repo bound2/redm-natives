f434d2140da22902c91b2a4489984c7407abec83
bound2 <9380102+bound2@users.noreply.github.com>
2020-05-30 23:48:27 +0300

    port over namespace 71

---
+++ src/module/Namespace71.module.ts
@@ -0,0 +1,26 @@
+declare function ShowAdvancedNotification(p2: number, p3: number): [any, any, any];
+declare function ShowLocationNotification(p2: number, p3: number): [any, any, any];
+declare function ShowNotification(p2: number, p3: number): [any, any, any];
+declare function ShowObjective(p2: number): [any, any, any];
+declare function ShowSimpleRightText(p2: number): [any, any, any];
+declare function ShowTooltip(p2: number): [any, any, any];
+// natives
+declare function N_0x02bcc0fe9eba3529(p0: any, p1: any, p2: any, p3: any, p4: any): any;
+declare function N_0x0fd07141ad048aae(p0: any, p1: any): any;
+declare function N_0x18d6869fbffec0f8(p0: any, p1: any, p2: any, p3: any): any;
+declare function N_0x2024f4f333095fb1(p0: any, p1: any, p2: any): any;
+declare function N_0x2f901291ef177b02(p0: any, p1: any): void;
+declare function N_0x3f9fddba79117c69(p0: any, p1: any, p2: any, p3: any): any;
+declare function N_0x4e88a65968a55c78(p0: any, p1: any): any;
+declare function N_0x59fa676177dbe4c9(p0: any): any;
+declare function N_0x6035e8fbca32ac5e(): void;
+declare function N_0x6d85126f6ccf02c9(p0: any, p1: any, p2: any): void;
+declare function N_0x860ddfe97cc94df0(p0: any, p1: any, p2: any): any;
+declare function N_0x893128cdb4b81fbb(p0: any, p1: any, p2: any): any;
+declare function N_0x8d1249bd28791878(p0: any, p1: any, p2: any): any;
+declare function N_0xa6f4216ab10eb08e(p0: any, p1: any, p2: any, p3: any): any;
+declare function N_0xaff5be9ba496ce40(p0: any, p1: any, p2: any, p3: any, p4: any): any;
+declare function N_0xb249ebcb30dd88e0(p0: any, p1: any, p2: any): any;
+declare function N_0xb7223b91cd6b7e07(p0: any): any;
+declare function N_0xc17f69e1418cd11f(p0: any): any;
+declare function N_0xc48152bc6b3e821c(p0: any, p1: any, p2: any): any;
