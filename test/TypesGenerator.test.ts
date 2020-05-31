3fc96549a985617244c790d8ad36e55a0a3a5ef5
bound2 <9380102+bound2@users.noreply.github.com>
2020-05-31 14:28:57 +0300

    refactor generator code a little bit

--- test/TypesGenerator.test.ts
+++ test/TypesGenerator.test.ts
@@ -3,58 +3,12 @@ import 'mocha';
 import { expect } from 'chai';
 import * as nativesJson from './natives.json'
 
-describe('Namespace71 module',
+describe('Module declaration generator',
     () => {
-        const ns = "_NAMESPACE71";
-        const nsJson = nativesJson[ns];
-        const nsFns = Object.keys(nsJson);
-
-        let hashNatives: Array<Generatable> = new Array();
-        let namedNatives: Array<Generatable> = new Array();
-        for (const fn of nsFns) {
-            // Construct native function name that's invokable from JS
-            let name = nsJson[fn].name;
-            const hashNative: boolean = !name;
-            if (hashNative) {
-                name = `N_${nsJson[fn]["hash"].toLowerCase()}`;
-            } else {
-                name = name.split("_").filter((p) => p).map((p) => capitalize(p)).reduce((a, b) => a + b);
-            }
-
-            // Gather returnable parameters
-            const params: Array<Parameter> = nsJson[fn]["params"];
-            const returnParams: Array<string> = params
-                .filter((p) => p.type.endsWith("\*"))
-                .map((p) => p.type.substring(0, p.type.length - 1))
-                .map((p) => type(p));
-            const returns: Array<string> = [type(nsJson[fn].results)].concat(returnParams);
-            let returnsString: string;
-            if (returns.length > 1) {
-                returnsString = `[${returns.join(", ")}]`;
-            } else {
-                returnsString = returns[0];
-            }
-
-            // Gather function parameters
-            const fnParams: Array<string> = params.filter((p) => !p.type.endsWith("\*")).map((p) => `${p.name}: ${type(p.type)}`);
-            const paramString: string = fnParams.join(", ");
-
-            const gen: Generatable = {
-                funcName: name,
-                funcParamString: paramString,
-                returnParamString: returnsString
-            };
-            if (hashNative) {
-                hashNatives.push(gen);
-            } else {
-                namedNatives.push(gen);
-            }
-        }
-
-        console.log("// Named functions");
-        namedNatives.sort((a, b) => a.funcName.localeCompare(b.funcName)).forEach((n) => console.log(toDeclaration(n)));
-        console.log("// Hash functions");
-        hashNatives.sort((a, b) => a.funcName.localeCompare(b.funcName)).forEach((n) => console.log(toDeclaration(n)));
+        it('namespace71', () => {
+            const ns = "_NAMESPACE71";
+            moduleDeclaration(ns, true, true);
+        });
     }
 );
 
@@ -70,6 +24,61 @@ interface Generatable {
     funcName: string;
     funcParamString: string;
     returnParamString: string;
+}
+
+function moduleDeclaration(ns: string, printNamed: boolean, printHashed: boolean): void {
+    const nsJson = nativesJson[ns];
+    const nsFns = Object.keys(nsJson);
+
+    let hashNatives: Array<Generatable> = new Array();
+    let namedNatives: Array<Generatable> = new Array();
+    for (const fn of nsFns) {
+        // Construct native function name that's invokable from JS
+        let name = nsJson[fn].name;
+        const hashNative: boolean = !name;
+        if (hashNative) {
+            name = `N_${nsJson[fn]["hash"].toLowerCase()}`;
+        } else {
+            name = name.split("_").filter((p) => p).map((p) => capitalize(p)).reduce((a, b) => a + b);
+        }
+
+        // Gather returnable parameters
+        const params: Array<Parameter> = nsJson[fn]["params"];
+        const returnParams: Array<string> = params
+            .filter((p) => p.type.endsWith("\*"))
+            .map((p) => p.type.substring(0, p.type.length - 1))
+            .map((p) => type(p));
+        const returns: Array<string> = [type(nsJson[fn].results)].concat(returnParams);
+        let returnsString: string;
+        if (returns.length > 1) {
+            returnsString = `[${returns.join(", ")}]`;
+        } else {
+            returnsString = returns[0];
+        }
+
+        // Gather function parameters
+        const fnParams: Array<string> = params.filter((p) => !p.type.endsWith("\*")).map((p) => `${p.name}: ${type(p.type)}`);
+        const paramString: string = fnParams.join(", ");
+
+        const gen: Generatable = {
+            funcName: name,
+            funcParamString: paramString,
+            returnParamString: returnsString
+        };
+        if (hashNative) {
+            hashNatives.push(gen);
+        } else {
+            namedNatives.push(gen);
+        }
+    }
+    if (printNamed) {
+        console.log("// Named functions");
+        namedNatives.sort((a, b) => a.funcName.localeCompare(b.funcName)).forEach((n) => console.log(toDeclaration(n)));
+    }
+    if (printHashed) {
+        console.log("// Hash functions");
+        hashNatives.sort((a, b) => a.funcName.localeCompare(b.funcName)).forEach((n) => console.log(toDeclaration(n)));
+    }
 }
 
 function toDeclaration(g: Generatable): string {
