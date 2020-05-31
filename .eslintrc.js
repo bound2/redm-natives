6bb8460ed6da970f490387df7beac66308b5489e
bound2 <9380102+bound2@users.noreply.github.com>
2020-05-31 16:12:02 +0300

    disable some lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
         'prettier/@typescript-eslint',
     ],
     rules: {
-        "semi": [1, "always"]
+        "semi": [1, "always"],
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/camelcase": "off"
     }
 };
